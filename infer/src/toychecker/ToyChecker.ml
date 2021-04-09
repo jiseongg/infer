d8f9a3566967f0e1663b311829821f403d2e0533
jiseongg <jiseongg301@gmail.com>
2021-04-09 07:15:42 +0000

    [ToyChecker] implement error reporting

--- infer/src/toychecker/ToyChecker.ml
+++ infer/src/toychecker/ToyChecker.ml
@@ -4,9 +4,13 @@ open! IStd
 module F = Format
 module L = Logging
 
+module CFG = ProcCfg.NormalOneInstrPerNode
+
+module Dom = ToyCheckerDomain
+
 module TransferFunctions = struct
   module CFG = ProcCfg.NormalOneInstrPerNode
-  module Domain = ToyCheckerDomain
+  module Domain = Dom
 
   type analysis_data = IntraproceduralAnalysis.t
   
@@ -45,42 +49,69 @@ module TransferFunctions = struct
   let pp_session_name _node fmt = F.pp_print_string fmt "ToyChecker"
 end
 
-module Analyzer = AbstractInterpreter.MakeWTO (TransferFunctions)
+module Analyzer = AbstractInterpreter.MakeRPO (TransferFunctions)
 
-let report_null_deref {IntraproceduralAnalysis.proc_desc; err_log} invariant_map =
-  let _report_null_deref node instr = 
-    let node_id = Procdesc.Node.get_id in
-    let instr_idx = ProcCfg.InstrNode.id
-    match Analyzer.extract_pre node invariant_map with
-    | Some pre_state -> (
-        match instr with
-        | Sil.Store {e1 = Exp.Var id; e2} -> (
-            match ToyCheckerDomain.find (Var.of_id id) pre_state with
-            | ToyCheckerDomain.Nullable ->
-                let loc = Procdesc.get_loc proc_desc in
-                let message = F.asprintf "Null dereference on %a" Ident.pp id in
-                Reporting.log_issue proc_desc err_log loc
-                  ToyChecker IssueType.toy_checker_err message
-            | _ -> ()
-          )
+let report_issues {IntraproceduralAnalysis.proc_desc; err_log} cfg invariant_map = 
+  let report_null_deref pre_state instr =
+    match instr with 
+    | Sil.Store {e1= Exp.Var id; e2; loc} -> (
+        match Dom.find_opt (Var.of_id id) pre_state with
+        | Some Dom.Nullable ->
+            let message = F.asprintf "Null dereference on %a" Ident.pp id in
+            Reporting.log_issue proc_desc err_log ~loc ToyChecker IssueType.toy_checker_err message
+        | _ -> ()
+      )
+    | Sil.Store {e1= Exp.Lvar pvar; e2; loc} -> (
+        match Dom.find_opt (Var.of_pvar pvar) pre_state with
+        | Some Dom.Nullable ->
+            let message = F.asprintf "Null dereference on %a"
+                (fun fmt -> Pvar.pp Pp.text fmt) pvar in
+            Reporting.log_issue proc_desc err_log ~loc ToyChecker IssueType.toy_checker_err message
+        | _ -> ()
+      )
+    | Sil.Load {id; e= Exp.Var _id; loc} -> (
+        match Dom.find_opt (Var.of_id _id) pre_state with
+        | Some Dom.Nullable ->
+            let message = F.asprintf "Null dereference on %a" Ident.pp _id in
+            Reporting.log_issue proc_desc err_log ~loc ToyChecker IssueType.toy_checker_err message
+        | _ -> ()
+      )
+    | Sil.Load {id; e= Exp.Lvar pvar; loc} -> (
+        match Dom.find_opt (Var.of_pvar pvar) pre_state with
+        | Some Dom.Nullable ->
+            let message = F.asprintf "Null dereference on %a"
+                (fun fmt -> Pvar.pp Pp.text fmt) pvar in
+            Reporting.log_issue proc_desc err_log ~loc ToyChecker IssueType.toy_checker_err message
         | _ -> ()
       )
     | _ -> ()
   in
-  Procdesc.iter_instrs _report_null_deref procdesc
+  let report_on_node node = 
+    let node_id = CFG.Node.id node in
+    let instrs = CFG.instrs node in
+    Instrs.iter instrs
+      ~f:(fun instr ->
+          match Analyzer.extract_pre node_id invariant_map with
+          | Some pre_state ->
+              report_null_deref pre_state instr
+          | None ->
+              () )
+  in
+  Container.iter cfg ~fold:CFG.fold_nodes ~f:report_on_node
 
 let checker ({IntraproceduralAnalysis.proc_desc; err_log} as analysis_data) =
   let pvars = Procdesc.get_pvar_formals proc_desc in
   let initialize_pvar astate = function
     | (pvar, {Typ.desc= Tptr _}) ->
-        ToyCheckerDomain.add (Var.of_pvar pvar) ToyCheckerDomain.Nullable astate
+        Dom.add (Var.of_pvar pvar) Dom.Nullable astate
     | (pvar, _) ->
-        ToyCheckerDomain.add (Var.of_pvar pvar) ToyCheckerDomain.NonNull astate
+        Dom.add (Var.of_pvar pvar) Dom.NonNull astate
   in
   let initial =
-    List.fold_left ~f:initialize_pvar ~init:ToyCheckerDomain.initial pvars 
+    List.fold_left ~f:initialize_pvar ~init:Dom.initial pvars 
   in
-  let invariant_map = Analyzer.exec_cfg
-      ~do_narrowing:false ~initial:initial proc_desc analysis_data in
-  report_null_deref analysis_data invariant_map
+  let cfg = CFG.from_pdesc proc_desc in
+  let invariant_map = Analyzer.exec_pdesc
+      ~do_narrowing:false ~initial analysis_data proc_desc in
+  report_issues analysis_data cfg invariant_map
 
